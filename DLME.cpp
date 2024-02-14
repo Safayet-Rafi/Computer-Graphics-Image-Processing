4ce42bc2034eb4ab9be7f60f5e43aa6436b06f79
Safayet Rafi <safayetrafi461@gmail.com>
2024-02-14 19:54:04 +0600

    when m==0 case handled

--- DLME.cpp
+++ DLME.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
-#include<map>
+#include<vector>
+#include<utility>
 #include<iterator>
 #include<cmath>
 using namespace std;
@@ -9,27 +10,32 @@ int main()
     //Input the two coordinate
     double x1,y1,x2,y2;
     cin>>x1>>y1>>x2>>y2;
+
+    vector<pair<int,int>>result;
+    result.push_back(make_pair(x1,y1));
     
     if(x2-x1==0)
     {
-        cout<<"It's a vertical line"<<endl;
-        return 0;
+        for(int i=y1+1;i<y2;i++)
+           result.push_back(make_pair(x1,i));
     }
+
+    else{
     double m = (y2-y1)/(x2-x1);//slope
     double b = y1 - (m*x1);//intercept
 
-    map<int,int>result;
-    result.insert({x1,y1});
-    result.insert({x2,y2});
     //storing the coordinate that will be turned on
     for(int i=x1+1;i<x2;i++)
     {
         double temp = round((m*i) + b);
-        result.insert({ i, temp }); 
+        result.push_back(make_pair(i,temp));
     }
+    }
+    result.push_back(make_pair(x2,y2));
+
     //printing the resulting coordinate
-    for (auto itr = result.begin(); itr != result.end(); ++itr) { 
-        cout << itr->first 
-             << '\t' << itr->second << '\n'; 
-    } 
+   for(int i=0;i<result.size();i++)
+   {
+       cout<<result[i].first<<"  "<<result[i].second<<endl;
+   }
 }
